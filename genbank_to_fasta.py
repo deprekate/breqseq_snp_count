ef38878e97a422808ac51d7d76e208a6c964319f
deprekate <deprekate@gmail.com>
2020-05-13 19:12:12 -0700

    Added genbank to fasta script

---
+++ genbank_to_fasta.py
@@ -0,0 +1,42 @@
+import sys
+import os.path
+
+
+def read_fasta(filepath):
+	my_contigs = dict()
+	name = ''
+	seq = ''
+	with open(filepath, mode="r") as my_file:
+		for line in my_file:
+			if(line.startswith(">")):
+				my_contigs[name] = seq
+				name = line.split()[0]
+				seq = ''
+			else:
+				seq += line.replace("\n", "").upper()
+		my_contigs[name] = seq
+
+	if '' in my_contigs: del my_contigs['']
+	return my_contigs
+
+header_line = ''
+flag = False
+with open(sys.argv[1]) as fp:
+	for line in fp:
+		if line.startswith('     CDS             '):
+			header_line = line.replace('CDS', '').strip()
+			flag = False
+		elif line.startswith('                     /locus_tag='):
+			header_line = line.replace('/locus_tag=', '').replace('"', '').strip()
+			flag = False
+		elif line.startswith('                     /translation='):
+			print(">", header_line, sep='')
+			print(line.replace('/translation=', '').replace('"', ''). strip())
+			flag = True
+		elif line.startswith('                     /'):
+			flag = False
+		elif line.startswith('ORIGIN'):
+			flag = False
+		elif flag:
+			print(line.replace('"', '').strip())
+
